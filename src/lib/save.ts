d05667a428988b5ffdde0edfe812548e58a67c0b
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-08-22 10:30:41 +0200

    feat: ownership

--- src/lib/save.ts
+++ src/lib/save.ts
@@ -2,13 +2,24 @@
 
 import { createClient } from '@/utils/supabase/server'
 
-export async function saveCode(code: string, instructions: string): Promise<string> {
+export async function saveCode(code: string, instructions: string, ownership: boolean, collaboration: boolean): Promise<string> {
   const supabase = await createClient()
   const uuid = crypto.randomUUID()
+  let owner: null | string | undefined = undefined
+  if (ownership) {
+    const { data: userData, error: userError } = await supabase.auth.getUser()
+    if (userError) {
+      owner = null
+    } else {
+      owner = userData.user.id
+    }
+  }
   const data = {
     code: code,
     instructions: instructions,
     shareCode: uuid,
+    codeOwner: owner ?? null,
+    allowsCollaboration: collaboration,
   }
 
   const { error } = await supabase.from("code").insert(data)
@@ -23,6 +34,24 @@ export async function saveCode(code: string, instructions: string): Promise<stri
 
 export async function updateCode(code: string, instructions: string, codeGetter: string): Promise<string> {
   const supabase = await createClient()
+  const { data: userData, error: userError } = await supabase.auth.getUser()
+
+  if (userError) {
+    return "An error occurred while trying to verify your permissions to write to this project. Try again later."
+  }
+
+  const currentUserId = userData.user.id
+
+  const { data: selectData, error: selectError } = await supabase.from("code").select("*").eq("shareCode", codeGetter)
+
+  if (selectError) {
+    return "An error occurred while trying to verify your permissions to write to this project. Try again later."
+  }
+
+  if (selectData[0].codeOwner != null && selectData[0].codeOwner != currentUserId && !selectData[0].allowsCollaboration) {
+    return "Sorry, it seems like you do not have permission to write this project"
+  }
+
   const data = {
     code: code,
     instructions: instructions,
